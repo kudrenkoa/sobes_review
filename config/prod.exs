2c096fd72bbae0ebfd3be69b1319976136381606
kudrenkoa <alex_kd@protonmail.com>
2019-12-19 18:53:51 +0300

    heroku deploy steps

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :sobes_review, SobesReviewWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "ancient-caverns-21201", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
